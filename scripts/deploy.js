3910fadc0da4b73ac36f50668f50f130fdd1a6c5
Jem <0x0xjem@gmail.com>
2024-05-29 12:16:29 +0400

    Update the manifest from the template before running codegen

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,8 @@
 const config = require("../deployment");
 const execute = require("./execute");
-const Mustache = require("mustache");
 
-// Read the values from the networks.json file
-const allNetworkValues = require("../networks.json");
-const { readFileSync, writeFileSync } = require("fs");
-const networkValues = allNetworkValues[config.TARGET_NETWORK];
-if (!networkValues) {
-  throw new Error(`No network values found in networks.json for ${config.TARGET_NETWORK}`);
-}
-
-// Add a "network" key
-networkValues.network = config.TARGET_NETWORK;
-
-console.log(`Generating subgraph.yaml for ${config.TARGET_NETWORK}\n`);
-
-// Read the subgraph template
-const subgraphTemplate = readFileSync("subgraph-template.yaml").toString();
-
-// Render the template with the network values
-const templatedSubgraphManifest = Mustache.render(subgraphTemplate, networkValues);
-
-// Write to file
-writeFileSync("subgraph.yaml", templatedSubgraphManifest);
+// Update the manifest
+require("./manifest");
 
 const buildCommand = `graph build`;
 
